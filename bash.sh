84bffebd95b16dbd2943824b201182c551233d4f
Oliver Michel <oliver.michel@editum.de>
2011-04-17 22:35:34 +0200

    add rvm version to ps1

--- bash.sh
+++ bash.sh
@@ -14,11 +14,21 @@ alias tree="tree -FAC"
 
 # command line customization
 
-parse_git_branch () {
-	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+parse_git_dirty() {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
 }
 
-export PS1="\[\033[0;32m\]\u@\h \[\033[0;36m\]\w\[\033[0m\] \[\033[0;35m\]\$(parse_git_branch)\[\033[0;38m\]\n> \$ "
+parse_git_branch() {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
+}
+
+rvm_version() {
+	if [ "$(~/.rvm/bin/rvm-prompt i v)" != "" ]; 
+		then echo "[$(~/.rvm/bin/rvm-prompt i)] "; 
+	fi
+}
+
+export PS1="\[\033[0;32m\]\u@\h \[\033[0;37m\]\$(rvm_version)\033[0m\]\[\033[0;36m\]\w\[\033[0m\] \[\033[0;35m\]\$(parse_git_branch)\[\033[0;38m\]\n> \$ "
 
 # bash calculator
 ? () { echo "$*" | bc -l; }
