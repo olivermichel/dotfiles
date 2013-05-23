afbf95f2e12ab70c656cad8f633725860b46e82f
Oliver Michel <oliver.michel@editum.de>
2013-05-23 19:40:48 +0200

    use history substring search and syntax highlighting for zsh

--- zshconfig.sh
+++ zshconfig.sh
@@ -11,6 +11,22 @@ setopt completealiases
 # make sure prompt substitution is enabled
 setopt prompt_subst
 
+# enable command correction
+setopt correct
+
+# use fish shell kike syntax highlighting
+source ~/.dotfiles/zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# use history substring completion
+source ~/.dotfiles/zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+# bind UP and DOWN arrow keys
+for keycode in '[' '0'; do
+  bindkey "^[${keycode}A" history-substring-search-up
+  bindkey "^[${keycode}B" history-substring-search-down
+done
+unset keycode
+
 PROMPT="%{$fg[green]%}%n@%m%{$reset_color%} %{$fg[cyan]%}%~%{$reset_color%} %{$fg[white]%}\$(rb_info)%{$reset_color%} %{$fg[white]%}\$(py_info)%{$reset_color%} %{$fg[magenta]%}\$(git_branch)%{$reset_color%}
 %# "
 RPROMPT="%{$fg[red]%}%(?..%?)%{$reset_color%}"
