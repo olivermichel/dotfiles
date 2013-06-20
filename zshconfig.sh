bdf2562883917da9fba8c19d8805be19c5c3347a
Oliver Michel <oliver.michel@editum.de>
2013-06-19 21:38:43 -0600

    configure zsh history in zshconfig file

--- zshconfig.sh
+++ zshconfig.sh
@@ -27,6 +27,12 @@ for keycode in '[' '0'; do
 done
 unset keycode
 
+# history configuration
+HISTSIZE=5000
+HISTFILE=~/.zshhistory
+SAVEHIST=5000
+
+# prompt configuration
 PROMPT="%{$fg[green]%}%n@%m%{$reset_color%} %{$fg[cyan]%}%~%{$reset_color%} %{$fg[white]%}\$(rb_info)%{$reset_color%} %{$fg[white]%}\$(py_info)%{$reset_color%} %{$fg[magenta]%}\$(git_branch)%{$reset_color%}
 %# "
 RPROMPT="%{$fg[red]%}%(?..%?)%{$reset_color%}"
