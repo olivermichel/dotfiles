9513ca36eecf75e4cfa35adacbf17d6b5b8646a4
Oliver Michel <oliver.michel@editum.de>
2020-04-26 12:55:06 +0200

    remove zsh ssh auto complete

--- sh/zsh/config.zsh
+++ sh/zsh/config.zsh
@@ -39,17 +39,3 @@ SAVEHIST=5000
 src(){cd ~/src/$1;}
 compctl -W ~/src/ -/ src
 
-# ssh autocomplete configuration
-
-h=($(echo $(history | awk '{print $4 " " $5 "\n"}' | grep 'ssh ' | awk '{print $2}' | sort -u)))
-if [[ -r ~/.ssh/config ]]; then
-  h=($h ${${${(@M)${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
-fi
-if [[ -r ~/.ssh/known_hosts ]]; then
-   h=($h ${${${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
-fi
-if [[ $#h -gt 0 ]]; then
-  zstyle ':completion:*:ssh:*' hosts $h
-  zstyle ':completion:*:slogin:*' hosts $h
-fi
-
